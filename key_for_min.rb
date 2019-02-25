86e1881fe34b3227213ce99cb8c15c77a26bd5e7
ADIL NAIMI <adilnm23@hotmail.com>
2019-02-25 08:12:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  stored_value=nil
+  stored_value=1000000
   answer=nil
   name_hash.collect do |name,value|
    
