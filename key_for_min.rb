4acfc8180af5d3fade12b2db11f4e9268ffa5102
ADIL NAIMI <adilnm23@hotmail.com>
2019-02-25 08:22:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  stored_value=:INFINITY
+  stored_value=Float::INFINITY
   answer=nil
   name_hash.collect do |name,value|
    
