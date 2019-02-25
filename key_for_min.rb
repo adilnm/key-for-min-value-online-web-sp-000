adeb45d2a8b009cb1e3d04c20cab6cf09a953982
ADIL NAIMI <adilnm23@hotmail.com>
2019-02-25 08:08:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.collect do |name,value|
    
     if(value<stored_value)
-      stored_value=name[value]
+      stored_value=value
       answer=name
     end
   end
